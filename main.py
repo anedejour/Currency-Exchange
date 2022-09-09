60ebfe2fec6e68f6a9a276dbf6923e162e28d87c
Raliane Mendes <ralianemc@gmail.com>
2022-09-09 10:30:48 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,81 @@
+
+
+def exchange_money(budget, exchange_rate):
+    """
+
+    :param budget: float - amount of money you are planning to exchange.
+    :param exchange_rate: float - unit value of the foreign currency.
+    :return: float - exchanged value of the foreign currency you can receive.
+    """
+
+    exchange = budget / exchange_rate
+
+    return exchange
+
+
+def get_change(budget, exchanging_value):
+    """
+
+    :param budget: float - amount of money you own.
+    :param exchanging_value: float - amount of your money you want to exchange now.
+    :return: float - amount left of your starting currency after exchanging.
+    """
+
+    money_before_exchange = budget - exchanging_value
+
+    return money_before_exchange
+
+def get_value_of_bills(denomination, number_of_bills):
+    """
+
+    :param denomination: int - the value of a bill.
+    :param number_of_bills: int - amount of bills you received.
+    :return: int - total value of bills you now have.
+    """
+
+    currently_bills = denomination * number_of_bills
+
+    return currently_bills
+
+
+def get_number_of_bills(budget, denomination):
+    """
+
+    :param budget: float - the amount of money you are planning to exchange.
+    :param denomination: int - the value of a single bill.
+    :return: int - number of bills after exchanging all your money.
+    """
+
+    bills_after_exchanging = int(budget) // int(denomination)
+
+    return bills_after_exchanging
+
+
+def get_leftover_of_bills(budget, denomination):
+    """
+
+    :param budget: float - the amount of money you are planning to exchange.
+    :param denomination: int - the value of a single bill.
+    :return: float - the leftover amount that cannot be exchanged given the current denomination.
+    """
+
+    leftover_of_bills = budget % denomination
+
+    return leftover_of_bills
+
+
+def exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
+
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
+    :param spread: int - percentage that is taken as an exchange fee.
+    :param denomination: int - the value of a single bill.
+    :return: int - maximum value you can get.
+    """
+
+    um = budget / (exchange_rate + exchange_rate * spread / 100)
+    dois = um // denomination
+    tres = dois * denomination
+
+    return tres
